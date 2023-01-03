e72b376517cc3a94a55d4387fc1cd7ee6e5d6c5f
JaimeApBa <38909325+JaimeApBa@users.noreply.github.com>
2023-01-03 17:27:59 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 // Database data connection
 // ==================================
 const connection = mysql.createPool({
-    host: process.env.HOST,
-    user: 'username',
-    password: process.env.PASSWORD || '',
-    database: process.env.DATABASE
+    host: process.env.HOST_PROD,
+    user: process.env.USER_PROD,
+    password: process.env.PASSWORD_PROD,
+    database: process.env.DATABASE_PROD
 
 });
 
